83ad5e42194e8bf13e8ec01de82937d337e03232
HansrajBissessur <hansrajbissessur@icloud.com>
2026-06-02 10:56:53 +0400

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://studio-raphus.github.io',
+});
